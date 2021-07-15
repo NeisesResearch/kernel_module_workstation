ee9fb3e28a4e1aca59da392d4d10d0de332f706b
Michael Neises <neisesmichael@gmail.com>
2021-07-15 18:39:19 -0500

    ensure clean working dir

--- getCamkesArmVm.sh
+++ getCamkesArmVm.sh
@@ -2,11 +2,12 @@
 # add the module_minimal app,
 
 #dir=camkesvm`date +"%B%d"`
-dir=camkes_arm_vm
-mkdir ${dir};
-cd ${dir} &&
+camkesdir=camkes-arm-vm
+mkdir ${camkesdir};
+
+cd ${camkesdir} &&
 echo "==================================" &&
-echo "Grab the latest repo..." &&
+echo "Grab the latest camkes-arm-vm repo..." &&
 echo "==================================" &&
 repo init -u https://github.com/sel4/camkes-arm-vm-manifest &&
 repo sync &&
@@ -18,23 +19,34 @@ cp -r ../module_minimal projects/vm-examples/apps/Arm/ &&
 echo "==================================" &&
 echo "module_minimal copied!" &&
 mkdir build &&
-cd build &&   
 echo "Copy scripts into the build directory..." &&
 echo "==================================" &&
-cp ../../buildScripts/* . &&
+cp ../buildScripts/* build/ &&
 echo "==================================" &&
 echo "Copied!" &&
 echo "Clone the Linux kernel and checkout the latest 4.9.y version..." &&
 echo "==================================" &&
+cd .. &&
+
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git &&
+
 cd linux-stable &&
 git checkout linux-4.9.y &&
 git fetch &&
 echo "==================================" &&
-echo "Got it!" &&
+echo "Cloned!" &&
+echo "Ensure the working directory is clean..." &&
+echo "==================================" &&
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- clean &&
+(rm .config || true) &&
+(rm .config.old || true) &&
+(rm Module.symvers || true) &&
+echo "==================================" &&
+echo "Clean!" &&
 echo "Grab the Linux config used in the camkes-vm-images project..." &&
 echo "==================================" &&
-cp ../camkes-arm-vm/projects/camkes-vm-images/qemu-arm-virt/linux-configs/config .config &&
+cp  ../${camkesdir}/projects/camkes-vm-images/qemu-arm-virt/linux_configs/config .config &&
+cp .config .config.old &&
 echo "==================================" &&
 echo "Found it!" &&
 echo "Prepare the kernel for cross compilation..." &&
@@ -42,15 +54,15 @@ echo "==================================" &&
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- prepare &&
 echo "==================================" &&
 echo "Prepared!" &&
-echo "Build the kernel. This will take a while. I'll try to beep when I'm done." &&
+echo "Build the kernel. This will take a while." &&
 echo "==================================" &&
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- &&
-(beep || true) &&
-echo "All finished! Now let's copy the image into the camkes-vm-images project." &&
-cp arch/arm64/boot/Image ../camkes-arm-vm/projects/camkes-vm-images/qemu-arm-virt/newLinux &&
+echo "==================================" &&
+echo "All finished! Now let's copy the new kernel image into the camkes-vm-images project." &&
+cp arch/arm64/boot/Image ../${camkesdir}/projects/camkes-vm-images/qemu-arm-virt/newLinux &&
 echo "Copy away the config and Modules.symvers into the camkes-vm-linux project." &&
-mkdir -p ../projects/camkes-vm-linux/linux-configs/4.9.y/64 &&
-cp .config ../projects/camkes-vm-linux/linux-configs/4.9.y/64/config &&
-cp Module.symvers ../projects/camkes-vm-linux/linux-configs/4.9.y/64/Module.symvers &&
+mkdir -p ../${camkesdir}/projects/camkes-vm-linux/linux_configs/4.9.y/64 &&
+cp .config ../${camkesdir}/projects/camkes-vm-linux/linux_configs/4.9.y/64/config &&
+cp Module.symvers ../${camkesdir}/projects/camkes-vm-linux/linux_configs/4.9.y/64/Module.symvers &&
 echo "Okay, all done! Now you can enter your Docker instance and build the camkes project setup for you in camkes-arm-vm/build."
 
